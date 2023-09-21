cd6289fd589629845f0ab331fb300250a94e5cd3
Xin Yi Li <61605285+lixin-source@users.noreply.github.com>
2023-09-21 12:35:16 -0400

    Fixed bounties '23

--- listicle/server/data/data.js
+++ listicle/server/data/data.js
@@ -4,7 +4,7 @@ const data = [
         "name": "Monkey D. Luffy",
         "position": "Captain",
         "image": "https://pa1.narvii.com/7372/2dd7195f1a84dc2ec24ad0b4b04e4d80a3e1830cr1-500-281_hq.gif",
-        "bounty": "320,000,000 Berries",
+        "bounty": "3,000,000,000 Berries",
         "description": "Luffy is the main protagonist and captain of the Straw Hat Pirates. He possesses the power of the Gomu Gomu no Mi, a Devil Fruit that grants him rubber-like abilities. Luffy is on a quest to become the Pirate King by finding the legendary One Piece treasure."
     },
     {
@@ -12,7 +12,7 @@ const data = [
         "name": "Roronoa Zoro",
         "position": "Swordsman",
         "image": "https://i.pinimg.com/originals/52/58/c8/5258c888ec199ec413bcaad606c9bada.gif",
-        "bounty": "320,000,000 Berries",
+        "bounty": "1,111,000,000 Berries",
         "description": "Zoro is the crew's swordsman and a master of the Three-Sword Style. He dreams of becoming the world's greatest swordsman and wields three swords in combat."
     },
     {
@@ -20,7 +20,7 @@ const data = [
         "name": "Nami",
         "position": "Navigator",
         "image": "https://i.pinimg.com/originals/5d/fa/52/5dfa52e60699074351cc6672807cf93e.gif",
-        "bounty": "66,000,000 Berries",
+        "bounty": "366,000,000 Berries",
         "description": "Nami is the crew's navigator and has exceptional map-reading and navigation skills. She dreams of creating a complete map of the world."
     },
     {
@@ -28,7 +28,7 @@ const data = [
         "name": "Usopp",
         "position": "Sniper",
         "image": "https://media.giphy.com/media/PbxvcZmtgGxTq/source.gif",
-        "bounty": "200 Berries (formerly 30,000,000 Berries)",
+        "bounty": "500,000,000 Berries",
         "description": "Usopp is the crew's sharpshooter and a skilled liar. He often tells tall tales and dreams of becoming a brave warrior of the sea."
     },
     {
@@ -36,7 +36,7 @@ const data = [
         "name": "Sanji",
       "position": "Chef",
       "image": "https://64.media.tumblr.com/d7675b57b36dc991c07bba92dadb5801/60b26ebd7d5dd644-39/s500x750/be9d320d436a5a045be37db78fc676360e0bda1e.gif",
-      "bounty": "177,000,000 Berries",
+      "bounty": "1,0320,000,000 Berries",
       "description":"Sanji is the crew's cook and a skilled martial artist. He uses his legs in combat and has a bounty due to his affiliation with the Straw Hat Pirates."
     },
     {
@@ -44,7 +44,7 @@ const data = [
         "name": "Tony Tony Chopper",
       "position": "Doctor",
       "image": "http://pa1.narvii.com/5818/8e458b101bac2d4f3b6c9119e087efe84678c450_hq.gif",
-      "bounty": "100 Berries (formerly 50 Berries)",
+      "bounty": "1000 Berries",
       "description": "Chopper is the crew's doctor and has the ability to transform into different forms thanks to the Human-Human Fruit. He's a kind-hearted reindeer."
     },
     {
@@ -52,7 +52,7 @@ const data = [
         "name": "Nico Robin",
       "position": "Archaeologist",
       "image": "https://s.aficionados.com.br/imagens/nico-robin-gif.gif",
-      "bounty": "130,000,000 Berries",
+      "bounty": "930,000,000 Berries",
       "description": "Robin is the crew's archaeologist and can create extra limbs thanks to the powers of the Flower-Flower Fruit. She's a scholar seeking historical knowledge."
     },
     {
@@ -60,7 +60,7 @@ const data = [
         "name": "Franky",
       "position": "Shipwright",
       "image": "http://24.media.tumblr.com/7b50fbb2dbee3161a65f2d58aaccad5b/tumblr_mfbetmqAES1r8kmk7o1_500.gif",
-      "bounty": "94,000,000 Berries",
+      "bounty": "394,000,000 Berries",
       "description": "Franky is the crew's shipwright and a cyborg with superhuman strength. He built the Thousand Sunny, the crew's ship."
     },
     {
@@ -68,7 +68,7 @@ const data = [
         "name": "Brook",
       "position": "Musician",
       "image": "https://images6.fanpop.com/image/photos/40200000/-Brook-one-piece-40251315-500-238.gif",
-      "bounty": "83,000,000 Berries",
+      "bounty": "383,000,000 Berries",
       "description": "Brook is the crew's musician and a living skeleton thanks to the Revive-Revive Fruit. He's also a skilled swordsman and can use the power of the Soul-Soul Fruit."
     },
     {
@@ -76,7 +76,7 @@ const data = [
         "name": "Jinbe",
       "position": "Helmsman",
       "image": "https://media.tenor.com/92P0V36zt0cAAAAC/jimbei-jinbe.gif",
-      "bounty": "438,000,000 Berries (formerly 76,000,000 Berries)",
+      "bounty": "1,100,000,000 Berries",
       "description": "Jinbe is the crew's helmsman and a fish-man with incredible strength and fish-man karate skills. He is the newest member of the crew and is known for his wisdom and calm demeanor."
     }
   ]
